7d9e5a5d48f05a91952d61a9e0224bb8e02d4dfe
StefaniaVitali <stefy.egypt@hotmail.it>
2024-07-25 01:49:17 +0200

    Initial commit

---
+++ src/test/java/it/svitali/dashboard/TicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package it.svitali.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
